1d9b53e9b42f0bb1eb081378f6dca53e16258562
r4xjs <github@lowsec.net>
2021-10-12 22:46:41 +0200

    basic container setup working with ssh jump host

---
+++ roles/ssh-access/templates/add-ssh-access.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+while ! ping -c 1 -W 2 8.8.8.8; do
+    sleep 1
+    echo waiting for the network
+done
+apt update -y
+apt install -y sudo openssh-server python3
+systemctl enable ssh
+systemctl start ssh
+useradd {{username}}
+gpasswd -a {{username}} sudo
+sed -i 's/%sudo.*/%sudo    ALL=(ALL:ALL) NOPASSWD:ALL/' /etc/sudoers
+mkdir -p /home/{{username}}/.ssh
+echo "{{ansible_ssh_key}}" >> /home/{{username}}/.ssh/authorized_keys
+chown -R {{username}}:{{username}} /home/{{username}}
+chmod 700 /home/{{username}}/.ssh
+
